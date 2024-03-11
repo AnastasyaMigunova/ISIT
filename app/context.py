c3e07f84815d1d7b9681807b71a3152e29ff2b25
AnastasyaMigunova <65sar.ru@gmail.com>
2024-03-11 23:16:06 +0400

    adding variables to config and .env

--- app/context.py
+++ app/context.py
@@ -1,14 +1,24 @@
 import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from ISIT.config import config
+
+load_dotenv()
 
 
 class Context:
     visitedPages = set()
-    startUrl = "https://wallpaperscraft.com"
-    directory = 'images'
+    startUrl = os.getenv("START_URL")
+    directory = os.getenv("DIRECTORY")
+    visited_urls = config.VISITED_URLS
+
+    engine = create_async_engine(os.getenv("DATABASE_URL"))
+    async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False, future=True)
 
     def init_visited_pages(self):
-        if os.path.exists("visited_urls.txt"):
-            with open("visited_urls.txt", "r") as f:
+        if os.path.exists(self.visited_urls):
+            with open(self.visited_urls, "r") as f:
                 self.visitedPages = set(f.read().splitlines())
 
     def make_directory(self):
--- app/main.py
+++ app/main.py
@@ -7,16 +7,12 @@ from dotenv import load_dotenv
 import httpx
 from PIL import Image
 from bs4 import BeautifulSoup
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from ISIT.app.context import Context
 from ISIT.app.model.imageData import ImageData
 from ISIT.config import config
 
 load_dotenv()
-
-
-engine = create_async_engine(os.getenv("DATABASE_URL"))
-async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False, future=True)
-
+ctx = Context()
 
 async def fetch(url):
     async with httpx.AsyncClient() as client:
@@ -47,7 +43,7 @@ async def compress_image(image, max_size=1024 * 1024):
     return image
 
 
-async def download_image(url, directory, semaphore):
+async def download_image(url, directory, semaphore, session):
     async with semaphore:
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
@@ -60,7 +56,7 @@ async def download_image(url, directory, semaphore):
                 with open(image_path, "wb") as f:
                     f.write(compressed_data)
 
-                async with async_session.begin() as session:
+                async with session.begin() as session:
                     try:
                         image = ImageData(id=checksum, data=image_path)
                         session.add(image)
@@ -70,14 +66,14 @@ async def download_image(url, directory, semaphore):
                         logging.error(f"Error adding image to the database: {e}")
 
 
-async def process_page(url, number, directory):
+async def process_page(url, number, directory, session):
     html = await fetch(url)
     image_urls = await extract_images_urls(html)
 
     tasks = []
     semaphore = asyncio.Semaphore(config.MAX_CONCURRENT_TASKS)
     for img_url in image_urls[:number]:
-        tasks.append(download_image(img_url, directory, semaphore))
+        tasks.append(download_image(img_url, directory, semaphore, session))
 
     currentNumber = number - len(image_urls)
     if currentNumber < 0:
@@ -87,12 +83,12 @@ async def process_page(url, number, directory):
     return currentNumber
 
 
-async def crawler(next_url, directory, visited_urls, count, number, start_url):
+async def crawler(next_url, directory, visited_urls, count, number, start_url, session):
     currentNumber = number
 
     if next_url not in visited_urls:
         try:
-            currentNumber = await process_page(next_url, number, directory)
+            currentNumber = await process_page(next_url, number, directory, session)
             visited_urls.add(next_url)
             with open('visited_urls.txt', "a") as f:
                 f.write(next_url + "\n")
@@ -109,7 +105,7 @@ async def crawler(next_url, directory, visited_urls, count, number, start_url):
         nextPage = soup.find("a", class_="pager__link", string=str(count))
         if nextPage:
             next_url = start_url + nextPage.get("href")
-            await crawler(next_url, directory, visited_urls, count, currentNumber, start_url)
+            await crawler(next_url, directory, visited_urls, count, currentNumber, start_url, session)
         else:
             logging.warning("Page not found")
     else:
@@ -117,9 +113,9 @@ async def crawler(next_url, directory, visited_urls, count, number, start_url):
         return
 
 
-async def main(next_url, directory, number: int, visited_urls, start_url):
+async def main(next_url, directory, number: int, visited_urls, start_url, session):
 
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    await crawler(next_url, directory, visited_urls, 1, number, start_url)
+    await crawler(next_url, directory, visited_urls, 1, number, start_url, session)
